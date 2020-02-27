75c6a6dcb6bcb1708e97f750bee6b8989d79f257
edjunma <edjunma@gmail.com>
2020-02-27 03:10:10 -0500

    update to text color, hamburger menu

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -61,7 +61,7 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            <span className="text-color-secondary">{name || 'Your Name'}</span>
             <br />
             {subtitle || "I'm the Unknown Developer."}
             <span className="text-color-main">Front-End </span>
